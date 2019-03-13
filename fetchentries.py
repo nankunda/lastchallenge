01e2bc1ae6f9d21f35a4edd4ec13c59c503a6c01
AineKiraboMbabazi <mbabaziainekirabo@gmail.com>
2019-03-13 18:40:56 +0200

    added code for fetch entries

---
+++ fetchentries.py
@@ -0,0 +1,13 @@
+from flask import jsonify
+from app import app
+from create_entry import entries
+
+
+@app.route("/")
+def hello():
+    return "Welcome to my dairy application", 200
+
+
+@app.route('/api/v1/entries')
+def fetch_entries():
+    return jsonify({'entries': entries}), 200
